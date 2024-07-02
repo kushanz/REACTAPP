d01c07a35e3122a00b84bed6a921d479e909c775
kushanz <kushanshanuka@yahoo.com>
2024-07-02 16:27:32 +0530

    init UI maxmillur

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const clickEvent = () => {
-    setCount((count) => count + 1);
-  };
-
   return (
     <>
       <Header />
-      <div>{count}</div>
-      <button className="btn btn-primary" onClick={clickEvent}>
-        CLick Me
-      </button>
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,23 @@
+import reactImg from '../assets/react-core-concepts.png';
+// import './Header.css';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 export default function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
-    <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Home
-          </a>
-          <button class="navbar-toggler">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
 }
